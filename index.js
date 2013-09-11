164aeeb9052f6dcb8b4da51fc698fa6e252b3935
Eivind Uggedal <eivind@uggedal.com>
2013-09-11 15:44:03 +0200

    Recursive
    Closes #2.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var path = require('path');
 var inherits = require('util').inherits;
 var Duplex = require('stream').Duplex;
@@ -9,16 +10,11 @@ if (!Duplex) Duplex = require('readable-stream').Duplex;
 function RecurseFuture (root) {
   Duplex.call(this, {objectMode: true});
 
-  this.root = root;
-
+  this.descriptors = {};
   this.inotify = new Inotify();
-  this.inotify.addWatch({
-    path: root,
-    watch_for: Inotify.IN_CLOSE_WRITE,
-    callback: this._handler.bind(this)
-  });
 
-  recurse(root).pipe(this);
+  this._watch(root);
+  this._recurse(root);
 }
 
 inherits(RecurseFuture, Duplex);
@@ -28,8 +24,64 @@ RecurseFuture.prototype.close = function () {
   this.push(null);
 };
 
-RecurseFuture.prototype._handler = function (event) {
-  this.push(path.join(this.root, event.name));
+RecurseFuture.prototype._watch = function (relname) {
+  var id = this.inotify.addWatch({
+    path: relname,
+    watch_for: Inotify.IN_CLOSE_WRITE | Inotify.IN_CREATE,
+    callback: this._eventHandler.bind(this)
+  });
+
+  this.descriptors[id] = relname;
+};
+
+RecurseFuture.prototype._eventHandler = function (event) {
+  if (event.mask & Inotify.IN_CLOSE_WRITE) {
+    this.push(this._relname(event));
+  } else if (event.mask & Inotify.IN_CREATE ) {
+    this._createHandler(event);
+  }
+};
+
+RecurseFuture.prototype._createHandler = function (event) {
+  var relname = this._relname(event);
+
+  fs.stat(relname, function(err, stat) {
+    if (err) {
+      this.emit('error', err);
+      return;
+    }
+
+    if (stat.isDirectory()) {
+      this._watch(relname);
+      this._recurse(relname);
+    }
+  }.bind(this));
+};
+
+RecurseFuture.prototype._relname = function (event) {
+  return path.join(this.descriptors[event.watch], event.name);
+};
+
+RecurseFuture.prototype._recurse = function (relname) {
+  var r = recurse(relname, {writefilter: this._writefilter.bind(this)});
+  var self = this;
+
+  r.on('data', function (chunk) {
+    this.push(chunk);
+  }.bind(this));
+
+  r.on('error', function (err) {
+    this.emit('error', err);
+  }.bind(this));
+};
+
+
+RecurseFuture.prototype._writefilter = function (relname, stat) {
+  if (stat.isDirectory()) {
+    this._watch(relname);
+  }
+
+  return !stat.isDirectory();
 };
 
 RecurseFuture.prototype._read = function () {
---
+++ test/recursive.js
@@ -0,0 +1,68 @@
+var rf = require('../');
+var fs = require('fs');
+var test = require('tap').test;
+var Writable = require('stream').Writable;
+var path = require('path');
+var testfs = require('testfs');
+
+if (!Writable) Writable = require('readable-stream').Writable;
+
+test('recursive dirs', function (t) {
+  t.plan(1);
+
+  var files = [
+    '1.txt',
+    '2.txt',
+    'sub/3.txt',
+    'sub/subsub/4.txt',
+    'sub/subsub/subsubsub/5.txt',
+    'sub/subsub/subsubsub/6.txt',
+    'sub2/7.txt',
+  ];
+
+  var futureFiles = [
+    'future.txt',
+    'future/8.txt',
+    'future/9.txt',
+    'future/futuresub/10.txt',
+    'future/futuresub/11.txt',
+    'sub/12.txt',
+    'sub/subsub/13.txt'
+  ];
+
+  var paths = files.slice(0);
+  paths.push('sub/subsub/empty/');
+
+  var prefix = 'recursive';
+
+  testfs(prefix, paths, function (err, rm) {
+    var recursive = rf('recursive');
+
+    var all = [];
+    var concat = new Writable;
+    concat._write = function (chunk, enc, next) {
+      all.push(chunk);
+      if (all.length == files.length + futureFiles.length) recursive.close();
+      next();
+    };
+    concat.on('finish',  function () {
+      t.equal(
+        all.sort().join('\n'),
+        files.concat(futureFiles).map(function (name) {
+          return path.join(prefix, name);
+        }).sort().join('\n')
+      );
+      rm();
+    });
+
+    recursive.pipe(concat);
+
+    setTimeout(function() {
+      fs.mkdirSync('recursive/future');
+      fs.mkdirSync('recursive/future/futuresub');
+      futureFiles.forEach(function(ff) {
+        fs.writeFileSync(path.join('recursive', ff), '');
+      });
+    }, 10);
+  });
+});
